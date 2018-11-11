df8b8454ad15940881577646307f5ee3bcbd47dd
unknown <lironarad1@gmail.com>
2018-11-11 08:34:51 +0200

    add critical points

--- src/myMath/LinePlotTest.java
+++ src/myMath/LinePlotTest.java
@@ -10,7 +10,9 @@ import de.erichseifert.gral.plots.XYPlot;
 import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D;
 import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.plots.points.SizeablePointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.GraphicsUtils;
 import myMath.Monom;
 import myMath.Polynom;
 
@@ -30,27 +32,35 @@ public class LinePlotTest extends JFrame {
 		p.add(m4);
 		p.add(m5);
 		System.out.println(p);
+		System.out.println("area above function and under the x-axis: " +Math.abs(p.area(p.root(-1, 0, 0.001), p.root(4, 5, 0.001), 0.001)));
+		System.out.println("critical points: \n" + 0.19389 + " \n" + 1.75260 + " \n" + 3.67851);
 
 
 		DataTable data1 = new DataTable(Double.class, Double.class);
 		DataTable data2 = new DataTable(Double.class, Double.class);
+		DataTable data3 = new DataTable(Double.class, Double.class);
 
 		for (double x = -2.0; x <= 6; x+=0.001) {
 			double y = p.f(x);
 			data2.add(x,y);
 		}
-		double x0 = p.root(-1, 0, 0.001);
-		double x1 = p.root(4,5, 0.001);
+		double x0 = p.root(-1, 0, 0.00001);
+		double x1 = p.root(4,5, 0.00001);
 		
 		for (double x = x0; x <= x1; x+=0.001) {
 			double y = p.f(x);
 			data1.add(x,y);
 		}
+		data3.add(0.19389,p.f(0.19389));
+		data3.add(1.75260,p.f(1.75260));
+		data3.add(3.67851,p.f(3.67851));
+		
 		
 		DataSeries barSeries = new DataSeries(data1, 0, 1);
 		DataSeries lineSeries = new DataSeries(data2, 0, 1);
+		DataSeries criticalPoints = new DataSeries(data3, 0, 1);
 
-		BarPlot plot = new BarPlot(barSeries, lineSeries);
+		BarPlot plot = new BarPlot(barSeries,lineSeries,criticalPoints);
 		getContentPane().add(new InteractivePanel(plot));
 		
 		// Change the color of all bars
@@ -63,6 +73,16 @@ public class LinePlotTest extends JFrame {
 		plot.setLineRenderers(lineSeries, lineRenderer);
 		// The default point renderer (BarRenderer) needs to be deactivated (or changed)
 		plot.setPointRenderers(lineSeries, null);
+		
+		PointRenderer sizeablePointRenderer = new SizeablePointRenderer();
+		sizeablePointRenderer.setColor(GraphicsUtils.deriveDarker(Color.BLUE));
+		plot.setPointRenderers(criticalPoints, sizeablePointRenderer);
+//		// Display the second data series as a line plot
+//		LineRenderer lineRenderer2 = new DefaultLineRenderer2D();
+//		lineRenderer2.setColor(Color.BLUE);
+//		plot.setLineRenderers(criticalPoints, lineRenderer2);
+//		// The default point renderer (BarRenderer) needs to be deactivated (or changed)
+//		plot.setPointRenderers(criticalPoints, null);
 
 	}
 
