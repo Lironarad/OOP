8e554fbe5a6970b23c77931e997fb3b6ad4bb063
unknown <lironarad1@gmail.com>
2018-11-11 11:00:45 +0200

    add description of the plot build

--- src/myMath/LinePlotTest.java
+++ src/myMath/LinePlotTest.java
@@ -35,22 +35,25 @@ public class LinePlotTest extends JFrame {
 		System.out.println("area above function and under the x-axis: " +Math.abs(p.area(p.root(-1, 0, 0.001), p.root(4, 5, 0.001), 0.001)));
 		System.out.println("critical points: \n" + 0.19389 + " \n" + 1.75260 + " \n" + 3.67851);
 
-
+		//data of f(x) = 0.2x^4-1.5x^3+3.0x^2-x-5
 		DataTable data1 = new DataTable(Double.class, Double.class);
+		//data of area area above function and under the x-axis
 		DataTable data2 = new DataTable(Double.class, Double.class);
+		//data of critical points
 		DataTable data3 = new DataTable(Double.class, Double.class);
 
 		for (double x = -2.0; x <= 6; x+=0.001) {
 			double y = p.f(x);
 			data2.add(x,y);
 		}
+		
 		double x0 = p.root(-1, 0, 0.00001);
 		double x1 = p.root(4,5, 0.00001);
-		
 		for (double x = x0; x <= x1; x+=0.001) {
 			double y = p.f(x);
 			data1.add(x,y);
 		}
+		
 		data3.add(0.19389,p.f(0.19389));
 		data3.add(1.75260,p.f(1.75260));
 		data3.add(3.67851,p.f(3.67851));
@@ -59,7 +62,8 @@ public class LinePlotTest extends JFrame {
 		DataSeries barSeries = new DataSeries(data1, 0, 1);
 		DataSeries lineSeries = new DataSeries(data2, 0, 1);
 		DataSeries criticalPoints = new DataSeries(data3, 0, 1);
-
+		
+		//build the plot
 		BarPlot plot = new BarPlot(barSeries,lineSeries,criticalPoints);
 		getContentPane().add(new InteractivePanel(plot));
 		
@@ -74,15 +78,11 @@ public class LinePlotTest extends JFrame {
 		// The default point renderer (BarRenderer) needs to be deactivated (or changed)
 		plot.setPointRenderers(lineSeries, null);
 		
+		// Display the third data series as a points
 		PointRenderer sizeablePointRenderer = new SizeablePointRenderer();
 		sizeablePointRenderer.setColor(GraphicsUtils.deriveDarker(Color.BLUE));
 		plot.setPointRenderers(criticalPoints, sizeablePointRenderer);
-//		// Display the second data series as a line plot
-//		LineRenderer lineRenderer2 = new DefaultLineRenderer2D();
-//		lineRenderer2.setColor(Color.BLUE);
-//		plot.setLineRenderers(criticalPoints, lineRenderer2);
-//		// The default point renderer (BarRenderer) needs to be deactivated (or changed)
-//		plot.setPointRenderers(criticalPoints, null);
+
 
 	}
 
